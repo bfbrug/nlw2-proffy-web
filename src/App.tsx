1f549e13944faa4051737690627c726e42e8d508
Bruno Fernandes <bfbrug@gmail.com>
2020-08-05 08:10:49 -0400

    Commit inicial, routes, eslint, prettier config

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import Landing from './pages/Landing';
+
+import './assets/styles/global.css';
+import Routes from './routes';
+
+const App: React.FC = () => <Routes />;
+
+export default App;
